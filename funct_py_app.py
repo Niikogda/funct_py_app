7de5dd63f824412296442f2743ff583b3910a0f4
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-24 20:11:58 +0300

    done_5

--- funct_py_app.py
+++ funct_py_app.py
@@ -1,1 +1,14 @@
 
+def capitalize_words(s):
+    words = s.split()
+    capitalized_words = [word.capitalize() for word in words]
+    return " ".join(capitalized_words)
+
+while True:
+    try:
+        s = input("Enter a string: ")
+        result = capitalize_words(s)
+        print(f"The string with capitalized words is: {result}")
+        break
+    except ValueError:
+        print("Invalid input")
