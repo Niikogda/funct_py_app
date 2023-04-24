39d779e3a532880f62bb0b6950c7fa26343f29d9
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-24 21:26:11 +0300

    done!

--- funct_py_app.py
+++ funct_py_app.py
@@ -8,8 +8,8 @@ def dot_product(list1, list2):
         for i in range(len(list1)):
             result += list1[i] * list2[i]
         return result
-
-# Приклад виклику функції
+   
+    
 try:
     list1 = list(map(int, input("Enter the first list of numbers separated by spaces: ").split()))
     list2 = list(map(int, input("Enter the second list of numbers separated by spaces: ").split()))
