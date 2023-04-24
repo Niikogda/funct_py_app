363395852c05b7667b2d69fe988b8c9e01b6076e
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-24 21:34:06 +0300

    done_7

--- funct_py_app.py
+++ funct_py_app.py
@@ -1,1 +1,14 @@
 
+def get_divisors(num):
+    divisors = []
+    for i in range(1, num+1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+
+try:
+    num = int(input("Enter an integer number: "))
+    print("The divisors of", num, "are:", get_divisors(num))
+except ValueError:
+    print("Error: input must be an integer.")
