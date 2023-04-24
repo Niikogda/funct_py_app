9b5bb8c8640798bd0cf9c83274f4a07e135148d5
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-24 21:42:06 +0300

    done_9

--- funct_py_app.py
+++ funct_py_app.py
@@ -1,1 +1,14 @@
 
+def get_first_last_three_chars(s):
+    if len(s) < 6:
+        return "Недостатньо символів"
+    else:
+        return s[:3] + s[-3:]
+
+
+try:
+    s = input("Enter a string: ")
+    result = get_first_last_three_chars(s)
+    print(result)
+except ValueError:
+    print("Error: input must be a valid string.")
