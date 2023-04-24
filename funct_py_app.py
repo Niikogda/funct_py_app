f5075f8b9126762671e24a4eeaa98d1eb3bcd886
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-24 19:02:39 +0300

    done_1

--- funct_py_app.py
+++ funct_py_app.py
@@ -1,1 +1,13 @@
-
+def average():
+    while True:  
+        try:
+            lst = input("enter nums: ")
+            lst=lst.split()
+            lst = [int(x) for x in lst]  
+            result = sum(lst) / len(lst)
+            print(result)
+            return result
+        except ValueError:
+            print(" mistake ")
+average()
+ 
