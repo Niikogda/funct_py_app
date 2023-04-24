f69b4fe0788d5178bb0b125cd462773ab983d9eb
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-24 19:57:01 +0300

    done_3

--- funct_py_app.py
+++ funct_py_app.py
@@ -1,1 +1,14 @@
 
+def unique_list():
+    while True:
+        try:
+            lst = input("enter list: ")
+            lst = lst.split()
+            unique_lst = []
+            for elem in lst:
+                if elem not in unique_lst:
+                   unique_lst.append(elem)
+            return unique_lst
+        except ValueError:
+            print("mistake")
+print(unique_list())
