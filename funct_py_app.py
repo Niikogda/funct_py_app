299477ac4a42d97282f6b9a513c6b681a39ffd31
Laptop <Laptop@DESKTOP-BVI07RL>
2023-04-24 19:14:43 +0300

    done_2

--- funct_py_app.py
+++ funct_py_app.py
@@ -1,1 +1,17 @@
-
+def sounds():
+    while True:
+        try:
+            list=input("enter your list pls: ")
+            cons=0
+            vow=0
+            for i in list.lower():
+                if i.isalpha():
+                    if i in "aeiou":
+                        vow +=1
+                    else:
+                        cons +=1
+            print("consonats: ", cons)
+            print("vowels", vow)
+        except ValueError:
+            print("MASTAKE")
+sounds()
